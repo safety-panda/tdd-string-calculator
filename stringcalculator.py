f591fb76222ba6bbb459c1352a91926a50220fd0
safety-panda <earmiffs+git@googlemail.com>
2022-03-03 16:08:20 +0000

    added tests and refactored to support multiple numbers input as string

--- stringcalculator.py
+++ stringcalculator.py
@@ -1,3 +1,12 @@
-def add(str: str) -> int:
-    if(len(str) == 0) : return 0
-    else: return int(str)
+from typing import List
+
+def add(string_input: str) -> int:
+    if(len(string_input) == 0) : return 0
+    else:
+        num_comma_split: List[str] = string_input.split(',')   
+        sum_result: int = 0
+
+        for i in range(len(num_comma_split)):
+            sum_result = sum_result + int(num_comma_split[i])
+
+        return sum_result
--- stringcalculator_test.py
+++ stringcalculator_test.py
@@ -9,3 +9,11 @@ class TestStringCalculator(unittest.TestCase):
     def test_one_number_will_return_itself(self):
         self.assertEqual(add('2'), 2)
         self.assertEqual(add('24'), 24)
+    
+    def test_add_two_numbers_separated_by_comma(self):
+        self.assertEqual(add('1,2'), 3)
+        self.assertEqual(add('21,9'), 30)
+
+    def test_add_supports_random_number_of_number_inputs(self):
+        self.assertEqual(add('1,3,9,6'), 19)
+        self.assertEqual(add('1,4,7,9,8,10,11,23'),73)
